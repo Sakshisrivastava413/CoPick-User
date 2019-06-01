030b3a9ec242d61df0581e1b85931f22bfa98e74
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-06-01 20:30:59 +0530

    injected geocode service and converted current latlng to address

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,22 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { GeocodeService } from "../services/geocode.service";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"]
 })
 export class Tab1Page {
-  title: string = 'My first AGM project';
+  title: string = "My first AGM project";
   lat: number = 51.678418;
   lng: number = 7.809007;
-  constructor() {
-    if (navigator)
-    {
-    navigator.geolocation.getCurrentPosition( pos => {
+  currentAdd: string;
+
+  constructor(private geocode: GeocodeService) {
+    if (navigator) {
+      navigator.geolocation.getCurrentPosition(pos => {
         this.lng = +pos.coords.longitude;
         this.lat = +pos.coords.latitude;
+        this.geocode
+          .reverseGeocode(this.lat, this.lng)
+          .then((res: any) => this.currentAdd = res.results[0].formatted_address);
       });
     }
   }
-
 }
