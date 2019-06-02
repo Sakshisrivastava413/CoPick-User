760e010ad131fe661e27f46b82b047ce984e0c10
sakshi srivastava <sakshisrivastava413@gmail.com>
2019-06-02 06:01:38 +0530

    added button to navigate to my orders page

--- src/app/pages/payment/payment.page.ts
+++ src/app/pages/payment/payment.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-payment',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
-
+  goToMyOrderDetailPage() {
+    this.router.navigate(['my-orders'])
+  }
 }
